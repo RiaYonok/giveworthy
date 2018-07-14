fb6779eb6a28c7e2d40b635438511f177d694262
sandman8654 <sandman8654@outlook.com>
2018-07-15 03:17:44 +0800

    add signup router

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,14 @@ import LoginWithEmail from '@components/auth/LoginWithEmail';
 import Home from '@components/Home';
 import Main from '@components/Main';
 import AuthenticatedRoute from '@routes/AuthenticatedRoute';
-
+import Signup from '@components/auth/Signup';
 class Routes extends Component {
   render() {
     return (
       <div>
         <Route exact path='/login' component={Login} />
         <Route exact path='/loginwithemail' component={LoginWithEmail} />
+        <Route exact path='/signup' component={Signup} />
         <Route exact path='/' component={Home} />
         <Switch>
           {/* <Redirect from='/' to='/dashboard' exact /> */}
