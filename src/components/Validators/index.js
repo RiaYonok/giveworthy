3724b32b8fee89b02c168c460bf3e39a83f03aa7
sandman8654 <sandman8654@outlook.com>
2018-07-17 01:08:29 +0800

    signup form validation

---
+++ src/components/Validators/index.js
@@ -0,0 +1,5 @@
+import { ValidatorComponent, ValidatorForm } from 'react-form-validator-core';
+import InputValidator from './InputValidator';
+exports.InputValidator = InputValidator;
+exports.ValidatorComponent = ValidatorComponent;
+exports.ValidatorForm = ValidatorForm;
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -15,6 +15,7 @@ import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import  {Link}  from  'react-router-dom';
 import Stepper from '@components/BarStepper';
+import { ValidatorForm, InputValidator} from '@components/Validators';
 
 
 const styles={
@@ -49,6 +50,7 @@ export class Signup extends PureComponent {
     this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
     this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange = prop => event => {
     this.setState({ [prop]: event.target.value });
@@ -59,7 +61,10 @@ export class Signup extends PureComponent {
   handleMouseDownPassword (event){
     event.preventDefault();
   };
+  handleSubmit(){
 
+      return false;
+  }
   render() {
     var steps=[];
     for(var i=0;i<5;i++) steps.push(false);
@@ -71,40 +76,56 @@ export class Signup extends PureComponent {
         <Typography variant="title" color="default" className="sub-header-desc" gutterBottom>
           Add your info below
         </Typography>
-        <FormControl style={styles.form} >
-          <InputLabel htmlFor="email">Email Address</InputLabel>
-          <Input
-            fullWidth
-            id="email"
-            type="email"
-            value={this.state.email}
-            onChange={this.handleChange('email')}
-         />
-        </FormControl>
-        <FormControl style={styles.form} >
-          <InputLabel htmlFor="adornment-password">Password</InputLabel>
-          <Input
-            fullWidth
-            id="adornment-password"
-            type={this.state.showPassword ? 'text' : 'password'}
-            value={this.state.password}
-            onChange={this.handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="Toggle password visibility"
-                  onClick={this.handleClickShowPassword}
-                  onMouseDown={this.handleMouseDownPassword}
-                >
-                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <Button type="button" variant="contained"  className="login-button email-signin-button">
-          Next
-        </Button>
+        <ValidatorForm
+                ref="form"
+                onSubmit={this.handleSubmit}
+                style={styles.form}
+                onError={errors => console.log(errors)}
+            >
+             
+            <InputValidator
+                fullWidth
+                id="email"
+                name="email"
+                ref="email"
+                formControlStyle={styles.form}
+                inputLabel="Email Address"
+                type="email"
+                value={this.state.email}
+                validators={['required', 'isEmail']}
+                errorMessages={['this field is required', 'email is not valid']}
+                onChange={this.handleChange('email')}
+            />
+        
+            <InputValidator
+                fullWidth
+                id="password"
+                name="password"
+                ref="password"
+                formControlStyle={styles.form}
+                inputLabel="Password"
+                type={this.state.showPassword ? 'text' : 'password'}
+                value={this.state.password}
+                validators={['required', 'minStringLength:8','maxStringLength:50']}
+                errorMessages={['this field is required', 'Password length must be more than 8', 'Password length must be less than 50']}
+                onChange={this.handleChange('password')}
+                endAdornment={
+                <InputAdornment position="end">
+                    <IconButton
+                    aria-label="Toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                    >
+                    {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                </InputAdornment>
+                }
+            />
+
+            <Button type="submit" variant="contained"  className="login-button email-signin-button">
+            Next
+            </Button>
+        </ValidatorForm>
         <Stepper steps={steps}/>
       </div>
     );
