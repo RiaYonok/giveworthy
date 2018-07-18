2966c7854fbf5901db3204434f8bcd5d05da4141
sandman8654 <sandman8654@outlook.com>
2018-07-18 08:10:07 +0800

    update validator

--- src/components/Validators/index.js
+++ src/components/Validators/index.js
@@ -1,5 +1,7 @@
 import { ValidatorComponent, ValidatorForm } from 'react-form-validator-core';
 import InputValidator from './InputValidator';
+import TextValidator from './TextValidator';
 exports.InputValidator = InputValidator;
+exports.TextValidator = TextValidator;
 exports.ValidatorComponent = ValidatorComponent;
 exports.ValidatorForm = ValidatorForm;
