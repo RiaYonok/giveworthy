5343910524466bd43e2458e8459ecc188f0c4e04
sandman8654 <sandman8654@outlook.com>
2018-07-24 04:03:30 +0800

    update local sotrage function

--- src/localStorage.js
+++ src/localStorage.js
@@ -10,25 +10,27 @@ export const localState = ()=>{
             return undefined;
         }else{
             var payload  = JSON.parse(persistedState);
-            const donations = payload.users.current.donationProfile.donations;
-            payload.users.current.donationProfile.donations = donations?fromJS(donations):List();
-            payload.users.current.donationProfile = new DonationProfile(payload.users.current.donationProfile);
-            payload.cause.cause.photoLinks = payload.cause.cause.photoLinks?fromJS(payload.cause.cause.photoLinks):List();
-            payload.cause.cause.tags = payload.cause.cause.tags?fromJS(payload.cause.cause.tags):List();
-            payload.cause.cause.adminIds = payload.cause.cause.adminIds?fromJS(payload.cause.cause.adminIds):List();
-            payload.cause.cause.donationIds = payload.cause.cause.donationIds?fromJS(payload.cause.cause.donationIds):List();
+            // const donations = payload.users.current.donationProfile.donations;
+            // payload.users.current.donationProfile.donations = donations?fromJS(donations):List();
+            // payload.users.current.donationProfile = new DonationProfile(payload.users.current.donationProfile);
+            // if (payload.cause.cause){
+            //     payload.cause.cause.photoLinks = payload.cause.cause.photoLinks?fromJS(payload.cause.cause.photoLinks):List();
+            //     payload.cause.cause.tags = payload.cause.cause.tags?fromJS(payload.cause.cause.tags):List();
+            //     payload.cause.cause.adminIds = payload.cause.cause.adminIds?fromJS(payload.cause.cause.adminIds):List();
+            //     payload.cause.cause.donationIds = payload.cause.cause.donationIds?fromJS(payload.cause.cause.donationIds):List();
+            // }
 
             payload = {
                 users:Map({
                     users: Map(payload.users.users),
                     isLoading: payload.users.isLoading,
-                    current: new User(payload.users.current)
+                    current: payload.users.current?User.fromJS(payload.users.current): new User()
                 }),
                 errors:Map(payload.errors),
                 questionnaires:Map(payload.questionnaires),
                 status:Map(payload.status),
                 cause:Map({
-                    cause:new Cause(payload.cause.cause)})
+                    cause:payload.cause.cause?Cause.fromJS(payload.cause.cause):new Cause()})
             }
             return payload;
         }
