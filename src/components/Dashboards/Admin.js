1b5c4612ff4d4cb7f4c4eb6993b160bab13d105a
sandman8654 <sandman8654@outlook.com>
2018-08-14 15:59:51 +0800

    update admin dashboard page

--- src/components/Dashboards/Admin.js
+++ src/components/Dashboards/Admin.js
@@ -4,6 +4,18 @@ import { hot } from 'react-hot-loader';
 import { withStyles } from '@material-ui/core/styles';
 import { createSelector } from 'reselect';
 import getCurrentUser from '@selectors/getCurrentUser';
+import UserAvatar from 'react-user-avatar';
+import avatar from '@assets/images/if_male_628288.svg';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import DoneIcon from '@material-ui/icons/Done';
+import { Button, Hidden } from '@material-ui/core';
+
 const styles= theme => ({
     graySection:{
         background:"#DDD",
@@ -17,6 +29,16 @@ const styles= theme => ({
         minHeight:300,
         maxWidth:1024
     },
+    postDesc:{
+      fontSize:20,
+      fontWeight:'400',
+      color:"#757575"
+    },
+    button:{
+      color:"#757575",
+      marginLeft:5,
+      marginRight:5,
+    }
 })
 export class AdminDashboard extends PureComponent {
 
@@ -27,16 +49,63 @@ export class AdminDashboard extends PureComponent {
     }
     this.renderCharityList = this.renderCharityList.bind(this);
   }
-  renderCharityList(){
+  renderCharityList(posts){
+    const {classes} = this.props;
     return (
-        <div></div>
+        <List>
+          {posts.map((item, id)=>{
+              return(
+                <ListItem
+                  key={id}
+                  role={undefined}
+                  dense
+                  button
+                  //onClick={this.handleToggle(value)}
+                  className={classes.listItem}
+                >
+                  <UserAvatar 
+                      size="48" 
+                      name={item.name} 
+                      src={item.imageURL} 
+                      colors={['#BDBDBD']} 
+                    />
+                  <ListItemText primary={item.name} className={classes.postDesc} disableTypography={true}/>
+                  <ListItemSecondaryAction>
+                    <Hidden xsDown>
+                      <Button variant="outlined" className={classes.button}>Approve</Button>
+                      <Button variant="outlined" className={classes.button}>Deny</Button>
+                    </Hidden>
+                    <Hidden  only={['sm', 'lg','xl','md']}>
+                      <IconButton aria-label="approve">
+                        <DoneIcon/>
+                      </IconButton>
+                      <IconButton aria-label="deny">
+                        <ClearIcon/>
+                      </IconButton>
+                    </Hidden>
+                  </ListItemSecondaryAction>
+                </ListItem>
+                
+              )
+            })}
+        </List>
     )
   }
   render() {
     const {classes,user} = this.props;
+    const posts =[{
+      imageUrl:avatar,
+      name:"InYaSchool",
+      type:"video"
+    },{
+      imageUrl:avatar,
+      name:"Allthenature",
+      type:"photo"
+    }];
     return (
       <div className="main-container">
         <div className={classes.graySection}>
+          {this.renderCharityList(posts)}
         </div>
       </div>
     );
